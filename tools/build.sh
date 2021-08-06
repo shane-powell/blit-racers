7cbdc7fda54a1e947d355d20fa0162d231b69a8f
Shane Powell <kenjara@hotmail.com>
2021-08-06 09:24:29 +0100

    Fix cmake path

--- tools/build.sh
+++ tools/build.sh
@@ -2,5 +2,5 @@ pip3 install --upgrade 32blit
 rm -r ../build.stm32/
 mkdir ../build.stm32
 cd ../build.stm32
-cmake .. -DCMAKE_TOOLCHAIN_FILE=../../32blit-beta/32blit.toolchain
+cmake .. -DCMAKE_TOOLCHAIN_FILE=../../32blit-sdk/32blit.toolchain
 make
